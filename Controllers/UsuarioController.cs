48af9379f1af4930597dea7d1db6aa656fb456d8
devsantos16 <devsantos1999@gmail.com>
2022-10-20 15:54:58 -0300

    Api example Controllers

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.net.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+            return Ok(obj);
+        }
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            return Ok(new {nome});
+        }
+    }
+}
