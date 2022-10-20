4ed3ec3cba67a2a61949f745b227e8fd15e8ab2f
devsantos16 <devsantos1999@gmail.com>
2022-10-20 20:01:59 -0300

    Fix, drop some trash

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -6,27 +6,16 @@ using Microsoft.AspNetCore.Mvc;
 using webapi.net.models;
 using webapi.net;
 
-
 namespace webapi.net.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class UsuarioController : ControllerBase
     {
-        [HttpGet("ObterDataHoraAtual")]
-        public IActionResult ObterDataHora()
-        {
-            var obj = new
-            {
-                Data = DateTime.Now.ToLongDateString(),
-                Hora = DateTime.Now.ToShortTimeString()
-            };
-            return Ok(obj);
-        }
         [HttpGet("Apresentar")]
-        public IActionResult Apresentar() 
+        public IActionResult Apresentar()
         {
             return Ok(new RandomPerson());
         }
-}
+    }
 }
